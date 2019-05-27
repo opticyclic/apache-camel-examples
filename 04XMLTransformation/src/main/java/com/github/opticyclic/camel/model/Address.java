8345b626b0fc7ef72ae0b3cd5f45c7ca6d3d4e53
opticyclic <opticyclic@gmail.com>
2019-05-27 00:49:53 -0400

    Add equals and hashcode to generated JAXB classes

--- 04XMLTransformation/src/main/java/com/github/opticyclic/camel/model/Address.java
+++ 04XMLTransformation/src/main/java/com/github/opticyclic/camel/model/Address.java
@@ -6,9 +6,16 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 import com.github.opticyclic.xml.SimpleToStringStrategy;
+import org.jvnet.jaxb2_commons.lang.Equals2;
+import org.jvnet.jaxb2_commons.lang.EqualsStrategy2;
+import org.jvnet.jaxb2_commons.lang.HashCode2;
+import org.jvnet.jaxb2_commons.lang.HashCodeStrategy2;
+import org.jvnet.jaxb2_commons.lang.JAXBEqualsStrategy;
+import org.jvnet.jaxb2_commons.lang.JAXBHashCodeStrategy;
 import org.jvnet.jaxb2_commons.lang.ToString2;
 import org.jvnet.jaxb2_commons.lang.ToStringStrategy2;
 import org.jvnet.jaxb2_commons.locator.ObjectLocator;
+import org.jvnet.jaxb2_commons.locator.util.LocatorUtils;
 
 
 /**
@@ -44,7 +51,7 @@ import org.jvnet.jaxb2_commons.locator.ObjectLocator;
     "postalCode",
     "country"
 })
-public class Address implements ToString2
+public class Address implements Equals2, HashCode2, ToString2
 {
 
     @XmlElement(required = true)
@@ -202,6 +209,116 @@ public class Address implements ToString2
      */
     public void setCountry(String value) {
         this.country = value;
+    }
+
+    public boolean equals(ObjectLocator thisLocator, ObjectLocator thatLocator, Object object, EqualsStrategy2 strategy) {
+        if ((object == null)||(this.getClass()!= object.getClass())) {
+            return false;
+        }
+        if (this == object) {
+            return true;
+        }
+        final Address that = ((Address) object);
+        {
+            String lhsAddressLine1;
+            lhsAddressLine1 = this.getAddressLine1();
+            String rhsAddressLine1;
+            rhsAddressLine1 = that.getAddressLine1();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "addressLine1", lhsAddressLine1), LocatorUtils.property(thatLocator, "addressLine1", rhsAddressLine1), lhsAddressLine1, rhsAddressLine1, (this.addressLine1 != null), (that.addressLine1 != null))) {
+                return false;
+            }
+        }
+        {
+            String lhsAddressLine2;
+            lhsAddressLine2 = this.getAddressLine2();
+            String rhsAddressLine2;
+            rhsAddressLine2 = that.getAddressLine2();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "addressLine2", lhsAddressLine2), LocatorUtils.property(thatLocator, "addressLine2", rhsAddressLine2), lhsAddressLine2, rhsAddressLine2, (this.addressLine2 != null), (that.addressLine2 != null))) {
+                return false;
+            }
+        }
+        {
+            String lhsCity;
+            lhsCity = this.getCity();
+            String rhsCity;
+            rhsCity = that.getCity();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "city", lhsCity), LocatorUtils.property(thatLocator, "city", rhsCity), lhsCity, rhsCity, (this.city!= null), (that.city!= null))) {
+                return false;
+            }
+        }
+        {
+            String lhsState;
+            lhsState = this.getState();
+            String rhsState;
+            rhsState = that.getState();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "state", lhsState), LocatorUtils.property(thatLocator, "state", rhsState), lhsState, rhsState, (this.state!= null), (that.state!= null))) {
+                return false;
+            }
+        }
+        {
+            String lhsPostalCode;
+            lhsPostalCode = this.getPostalCode();
+            String rhsPostalCode;
+            rhsPostalCode = that.getPostalCode();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "postalCode", lhsPostalCode), LocatorUtils.property(thatLocator, "postalCode", rhsPostalCode), lhsPostalCode, rhsPostalCode, (this.postalCode!= null), (that.postalCode!= null))) {
+                return false;
+            }
+        }
+        {
+            String lhsCountry;
+            lhsCountry = this.getCountry();
+            String rhsCountry;
+            rhsCountry = that.getCountry();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "country", lhsCountry), LocatorUtils.property(thatLocator, "country", rhsCountry), lhsCountry, rhsCountry, (this.country!= null), (that.country!= null))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(Object object) {
+        final EqualsStrategy2 strategy = JAXBEqualsStrategy.INSTANCE;
+        return equals(null, null, object, strategy);
+    }
+
+    public int hashCode(ObjectLocator locator, HashCodeStrategy2 strategy) {
+        int currentHashCode = 1;
+        {
+            String theAddressLine1;
+            theAddressLine1 = this.getAddressLine1();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "addressLine1", theAddressLine1), currentHashCode, theAddressLine1, (this.addressLine1 != null));
+        }
+        {
+            String theAddressLine2;
+            theAddressLine2 = this.getAddressLine2();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "addressLine2", theAddressLine2), currentHashCode, theAddressLine2, (this.addressLine2 != null));
+        }
+        {
+            String theCity;
+            theCity = this.getCity();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "city", theCity), currentHashCode, theCity, (this.city!= null));
+        }
+        {
+            String theState;
+            theState = this.getState();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "state", theState), currentHashCode, theState, (this.state!= null));
+        }
+        {
+            String thePostalCode;
+            thePostalCode = this.getPostalCode();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "postalCode", thePostalCode), currentHashCode, thePostalCode, (this.postalCode!= null));
+        }
+        {
+            String theCountry;
+            theCountry = this.getCountry();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "country", theCountry), currentHashCode, theCountry, (this.country!= null));
+        }
+        return currentHashCode;
+    }
+
+    public int hashCode() {
+        final HashCodeStrategy2 strategy = JAXBHashCodeStrategy.INSTANCE;
+        return this.hashCode(null, strategy);
     }
 
     public String toString() {
--- 04XMLTransformation/src/main/java/com/github/opticyclic/camel/model/Employee.java
+++ 04XMLTransformation/src/main/java/com/github/opticyclic/camel/model/Employee.java
@@ -7,9 +7,16 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import com.github.opticyclic.xml.SimpleToStringStrategy;
+import org.jvnet.jaxb2_commons.lang.Equals2;
+import org.jvnet.jaxb2_commons.lang.EqualsStrategy2;
+import org.jvnet.jaxb2_commons.lang.HashCode2;
+import org.jvnet.jaxb2_commons.lang.HashCodeStrategy2;
+import org.jvnet.jaxb2_commons.lang.JAXBEqualsStrategy;
+import org.jvnet.jaxb2_commons.lang.JAXBHashCodeStrategy;
 import org.jvnet.jaxb2_commons.lang.ToString2;
 import org.jvnet.jaxb2_commons.lang.ToStringStrategy2;
 import org.jvnet.jaxb2_commons.locator.ObjectLocator;
+import org.jvnet.jaxb2_commons.locator.util.LocatorUtils;
 
 
 /**
@@ -59,7 +66,7 @@ import org.jvnet.jaxb2_commons.locator.ObjectLocator;
     "position"
 })
 @XmlRootElement(name = "employee")
-public class Employee implements ToString2
+public class Employee implements Equals2, HashCode2, ToString2
 {
 
     protected long id;
@@ -182,6 +189,102 @@ public class Employee implements ToString2
      */
     public void setPosition(String value) {
         this.position = value;
+    }
+
+    public boolean equals(ObjectLocator thisLocator, ObjectLocator thatLocator, Object object, EqualsStrategy2 strategy) {
+        if ((object == null)||(this.getClass()!= object.getClass())) {
+            return false;
+        }
+        if (this == object) {
+            return true;
+        }
+        final Employee that = ((Employee) object);
+        {
+            long lhsId;
+            lhsId = this.getId();
+            long rhsId;
+            rhsId = that.getId();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "id", lhsId), LocatorUtils.property(thatLocator, "id", rhsId), lhsId, rhsId, true, true)) {
+                return false;
+            }
+        }
+        {
+            String lhsFirstName;
+            lhsFirstName = this.getFirstName();
+            String rhsFirstName;
+            rhsFirstName = that.getFirstName();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "firstName", lhsFirstName), LocatorUtils.property(thatLocator, "firstName", rhsFirstName), lhsFirstName, rhsFirstName, (this.firstName!= null), (that.firstName!= null))) {
+                return false;
+            }
+        }
+        {
+            String lhsLastName;
+            lhsLastName = this.getLastName();
+            String rhsLastName;
+            rhsLastName = that.getLastName();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "lastName", lhsLastName), LocatorUtils.property(thatLocator, "lastName", rhsLastName), lhsLastName, rhsLastName, (this.lastName!= null), (that.lastName!= null))) {
+                return false;
+            }
+        }
+        {
+            Address lhsAddress;
+            lhsAddress = this.getAddress();
+            Address rhsAddress;
+            rhsAddress = that.getAddress();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "address", lhsAddress), LocatorUtils.property(thatLocator, "address", rhsAddress), lhsAddress, rhsAddress, (this.address!= null), (that.address!= null))) {
+                return false;
+            }
+        }
+        {
+            String lhsPosition;
+            lhsPosition = this.getPosition();
+            String rhsPosition;
+            rhsPosition = that.getPosition();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "position", lhsPosition), LocatorUtils.property(thatLocator, "position", rhsPosition), lhsPosition, rhsPosition, (this.position!= null), (that.position!= null))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(Object object) {
+        final EqualsStrategy2 strategy = JAXBEqualsStrategy.INSTANCE;
+        return equals(null, null, object, strategy);
+    }
+
+    public int hashCode(ObjectLocator locator, HashCodeStrategy2 strategy) {
+        int currentHashCode = 1;
+        {
+            long theId;
+            theId = this.getId();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "id", theId), currentHashCode, theId, true);
+        }
+        {
+            String theFirstName;
+            theFirstName = this.getFirstName();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "firstName", theFirstName), currentHashCode, theFirstName, (this.firstName!= null));
+        }
+        {
+            String theLastName;
+            theLastName = this.getLastName();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "lastName", theLastName), currentHashCode, theLastName, (this.lastName!= null));
+        }
+        {
+            Address theAddress;
+            theAddress = this.getAddress();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "address", theAddress), currentHashCode, theAddress, (this.address!= null));
+        }
+        {
+            String thePosition;
+            thePosition = this.getPosition();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "position", thePosition), currentHashCode, thePosition, (this.position!= null));
+        }
+        return currentHashCode;
+    }
+
+    public int hashCode() {
+        final HashCodeStrategy2 strategy = JAXBHashCodeStrategy.INSTANCE;
+        return this.hashCode(null, strategy);
     }
 
     public String toString() {
